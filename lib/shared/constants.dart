8cdee26856175f6083a4360b6c1011aa88e578ba
Geetanshu24 <geetanshubansal2001@gmail.com>
2023-04-06 01:34:14 -0700

    Commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+
+class Constants {
+  static String appId = "1:421318471890:web:1792698b5bafd83d79fd0b";
+  static String apiKey = "AIzaSyCfD5Oj9bo5HH4SPtqBc4CzANQRDGonraQ";
+  static String messagingSenderId = "421318471890";
+  static String projectId = "chat-application-64500";
+  final primaryColor = const Color(0xFFee7b64);
+}
