953d9260b6870d050977bf00ad5cad1abcb238bf
Geetanshu24 <geetanshubansal2001@gmail.com>
2023-04-06 02:01:48 -0700

    Commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_application/service/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,10 +9,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  AuthService authService = AuthService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Home Page")),
+      body: Center(
+          child: ElevatedButton(
+        child: Text("Log Out"),
+        onPressed: () {
+          authService.signOut();
+        },
+      )),
     );
   }
 }
